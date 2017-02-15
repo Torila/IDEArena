591ed222c16423553b56d7794c5564c20cec0000
Torila <victoria.laughlin@gmail.com>
2017-02-15 15:25:16 -0500

    Delete db_connect.php

--- includes/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$host = "localhost";
-	$user = "vnlaughlin";
-	$passwd = "cs480";
-	$database =  "vnlaughlin";
-	$connect = mysql_connect($host, $user, $passwd);
-	mysql_select_db($database);
-?>
